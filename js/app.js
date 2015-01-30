f53498c0bc3d5e7a08dcc29df324c88d08ae9e17
Rajesh Duggal <rajesh@duggalmail.com>
2015-01-30 17:05:45 -0500

    Add post editing feature

--- js/app.js
+++ js/app.js
@@ -29,3 +29,17 @@ App.PostsRoute = Ember.Route.extend({
     return posts;
   }
 });
+
+App.PostController = Ember.ObjectController.extend({
+  isEditing: false,
+
+  actions: {
+    edit: function() {
+      this.set('isEditing', true);
+    },
+
+    doneEditing: function() {
+      this.set('isEditing', false);
+    }
+  }
+});
